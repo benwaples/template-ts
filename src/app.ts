a3ef7d91f89043b54c3e6bd63c4feb159e31be9c
benwaples <benwaples@gmail.com>
2021-03-16 11:41:37 -0700

    functions, html, ts listener

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
 // import functions and grab DOM elements
+import { getRandomThrow } from './get-random-throw'
+const usersOptions = document.querySelector('#users-options')
+const playButton = document.querySelector('#play')
+const resultsSpan = document.querySelector('#results')
+const tallySpan = document.querySelector('#tally')
 
 // initialize state
+let computersThrow = getRandomThrow()
+let wins = 0;
+let losses = 0;
+let games = 0;
 
 // set event listeners to update state and DOM 
+playButton.addEventListener('click', () => {
+  const usersPlay: HTMLInputElement = usersOptions.querySelector('input:checked')
+
+  
+})
+
